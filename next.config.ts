14927c170d211e42396520d096028743ffbd23bf
Eloane Ducrocq <elo.ane.ducrocq@gmail.com>
2025-12-14 17:14:46 +0100

    correct PWA

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+module.exports = withPWA({
+  // Tes autres configs Next.js ici
+})
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
